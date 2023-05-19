298be2ec6c71bef70aa0075c94a567bd96ccc022
kodexx <65950405+pintoedu@users.noreply.github.com>
2023-05-19 11:27:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from savings_account import SavingsAccount
 from checking_account import CheckingAccount
 
 
-
 # displays options for selecting an account type
 def display_bank_menu():
 	print("\nBank Menu:")
