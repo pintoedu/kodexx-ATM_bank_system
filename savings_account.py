28b1aa47e354415cdd77b1c32697f876fdf93db9
kodexx <65950405+pintoedu@users.noreply.github.com>
2023-05-19 11:28:23 -0400

    Update savings_account.py

--- savings_account.py
+++ savings_account.py
@@ -2,7 +2,6 @@ from bank_account import BankAccount
 
 
 
-
 # SavingsAccount inherits from BankAccount
 class SavingsAccount(BankAccount):
 	# initializes the object with the starting balance and annual interest rate
