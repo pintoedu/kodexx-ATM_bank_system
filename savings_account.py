b50473a7a689d7ee030b15dcf1f83fd7ec059354
kodexx <65950405+pintoedu@users.noreply.github.com>
2023-06-12 14:03:14 -0400

    Update savings_account.py

--- savings_account.py
+++ savings_account.py
@@ -6,7 +6,6 @@ class SavingsAccount(BankAccount):
 	def __init__(self, starting_balance, annual_interest_rate):
 		super().__init__(starting_balance, annual_interest_rate)
 		
-
 	# check_status() checks the account status by comparing the current balance of the account to 25
 	def check_status(self):
 		if self._current_balance_this_month < 25:
