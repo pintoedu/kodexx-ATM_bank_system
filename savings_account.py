96ea616c534c089d9b8b0abcff6ed95f3aaaac14
kodexx <65950405+pintoedu@users.noreply.github.com>
2023-06-12 14:02:47 -0400

    Update savings_account.py

--- savings_account.py
+++ savings_account.py
@@ -8,7 +8,6 @@ class SavingsAccount(BankAccount):
 		
 		
 
-
 	# check_status() checks the account status by comparing the current balance of the account to 25
 	def check_status(self):
 		if self._current_balance_this_month < 25:
